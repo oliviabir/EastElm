6fd91e5b0bc21fe2f4b916b4f3319ac921d75fbc
olivia <oliviabir19@gmail.com>
2022-07-08 15:20:06 -0700

    edit review functionality complete

--- app/forms/edit_review_form.py
+++ app/forms/edit_review_form.py
@@ -3,7 +3,7 @@ from wtforms import TextAreaField, IntegerField, SubmitField
 from sqlalchemy import DateTime
 from wtforms.validators import DataRequired
 
-class CreateReviewForm(FlaskForm):
+class EditReviewForm(FlaskForm):
     user_id = IntegerField('User Id')
     product_id = IntegerField('Product Id')
     rating = IntegerField('Rating', validators=[DataRequired()])
