69e85bd1328bd86a1ddf74056577c3a47458c34f
olivia <oliviabir19@gmail.com>
2022-07-13 17:42:24 -0700

    logout button lightly styled

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
+import './LogoutButton.css'
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
@@ -13,7 +14,7 @@ const LogoutButton = () => {
     history.push('/')
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <button onClick={onLogout} className='logout-button'>Logout</button>;
 };
 
 export default LogoutButton;
